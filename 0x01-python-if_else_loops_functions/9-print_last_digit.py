5a0811fdf087f7623d9d4b41ed5328498917964d
GO-Shoderu <go.shoderu@gmail.com>
2024-03-12 10:41:40 -0700

    Pushing 9. There are only 3 colors, 10 digits, and 7 notes; it's what we do with them that's important

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+def print_last_digit(number):
+    if (abs(number) % 10):
+        print("{}".format(abs(number) % 10), end='')
+    else:
+        print("{}".format(0), end='')
+    return (abs(number) % 10)
