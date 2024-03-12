8114204c4f5cf41f4437378c14d4d6384c5ecb1c
GO-Shoderu <go.shoderu@gmail.com>
2024-03-11 19:26:16 -0700

    Pushing 11. Hello, write

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
-import sys 
-sys.stderr.write(f"and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+import sys
+from datetime import datetime
+
+date = datetime(2015, 10, 19)
+
+sys.stderr.write(f"and that piece of art is useful - Dora Korpar, {date:%Y-%m-%d}\n")
 sys.exit(1)
