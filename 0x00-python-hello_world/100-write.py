6bde892eac6234c37833a0076e49c64245f9c7d1
GO-Shoderu <go.shoderu@gmail.com>
2024-03-12 08:09:24 -0700

    Fixed styling error

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -4,5 +4,6 @@ from datetime import datetime
 
 date = datetime(2015, 10, 19)
 
-sys.stderr.write(f"and that piece of art is useful - Dora Korpar, {date:%Y-%m-%d}\n")
+sys.stderr.write(f"and that piece of art is useful - Dora \
+        Korpar, {date:%Y-%m-%d}\n")
 sys.exit(1)
