3e9bf3ba1e3f9bb412db85e7e374326b90b18459
GO-Shoderu <go.shoderu@gmail.com>
2024-03-14 16:12:58 -0700

    3. Infinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """add infinite number on cLD arg"""
+    import sys
+
+    argv = sys.argv
+    arg = len(argv) - 1
+    
+    sum = 0
+
+    if arg == 0:
+        print(sum)
+    else:
+        for i in range(1, len(argv)):
+            sum += int(argv[i])
+        print(sum)
