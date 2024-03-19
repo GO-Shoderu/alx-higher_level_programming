72014af1d5facd7072f67f76fcd1d18026f6fb89
GO-Shoderu <go.shoderu@gmail.com>
2024-03-18 20:50:32 -0700

    6. Lists of lists = Matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 
 def print_matrix_integer(matrix=[[]]):
     if len(matrix) == 0 or matrix is None:
-        return (None)
+        return
 
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
