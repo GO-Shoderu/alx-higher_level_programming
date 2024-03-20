04bcca736235b88a730cea444d916e6f0ceca29c
GO-Shoderu <go.shoderu@gmail.com>
2024-03-20 14:36:32 -0700

    10. Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    high = a_dictionary[next(iter(a_dictionary))]
-    key = ''
+    key = next(iter(a_dictionary))
+    high = a_dictionary[key]
 
     if a_dictionary is None or len(a_dictionary) == 0:
         return (None)
