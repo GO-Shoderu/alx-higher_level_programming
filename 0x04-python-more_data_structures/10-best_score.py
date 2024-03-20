0bf2ea150cf9ceefcfb0d1ff30a69de1cbaa9b17
GO-Shoderu <go.shoderu@gmail.com>
2024-03-20 14:33:33 -0700

    10. Best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    high = next(iter(a_dictionary))
+    key = ''
+
+    if a_dictionary is None or len(a_dictionary) == 0:
+        return (None)
+
+    for elem in a_dictionary:
+        if a_dictionary[elem] > high:
+            high = a_dictionary[elem]
+            key = elem
+
+    return (key)
